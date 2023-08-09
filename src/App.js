eeabad7abbd3353360f2ccbb36cdb19e7389b0ac
Daphne Balane <balanedaphne20@gmail.com>
2023-08-09 14:59:25 +0800

    (React-exam): Added Hello and Cars component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+
+function Hello() {
+  const [inputText, setInputText] = useState('');
+  const [displayText, setDisplayText] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    setDisplayText(inputText);
+  };
+
+  return (
+    <div>
+      <p>Enter your name: {displayText}</p>
+      <input
+        type="text"
+        placeholder="your name"
+        value={inputText}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleButtonClick}>Submit</button>
+    </div>
+  );
+}
+
+const Cars = () => {
+  const cars = ['Ford', 'BMW', 'Audi'];
+
+  return (
+    <div>
+      <h1>Who lives in my garage?</h1>
+      <ul>
+        {cars.map((car, index) => (
+          <li key={index}>I am a {car}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello />
+      <Cars />
     </div>
   );
 }
